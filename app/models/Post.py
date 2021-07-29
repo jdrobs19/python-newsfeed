092ca7b1c3856a2cefc81bddb6eadfc9f4490499
Jordan Roberts <jdrobs19@gmail.com>
2021-07-29 09:45:38 -0600

    Vote model complete. all models completed

--- app/models/Post.py
+++ app/models/Post.py
@@ -1,7 +1,11 @@
 from datetime import datetime
+
+from sqlalchemy.sql.expression import select
+from sqlalchemy.sql.functions import func
 from app.db import Base
+from .Vote import Vote
 from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
-from sqlalchemy.orm import relationship
+from sqlalchemy.orm import relationship,column_property
 
 class Post(Base):
     __tablename__ = 'posts'
@@ -13,3 +17,7 @@ class Post(Base):
     updated_at = Column(DateTime, default=datetime.now, onupdate=datetime.now)
     user = relationship('User')
     comments = relationship('Comment', cascade='all,delete')
+    votes = relationship('Vote', cascade='all,delete')
+    vote_count = column_property(
+        select([func.count(Vote.id)]).where(Vote.post_id == id)
+    )
